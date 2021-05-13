96bab697d8804f82688af5c59f947c70a3bb8c94
Darryl Bidulock <darryl@arterys.com>
2021-05-13 14:39:45 -0600

    Build script to move stuff out of the workflow

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+
+# Config settings
+GITHUB_REPOSITORY=${GITHUB_REPOSITORY-arterys/status-page}
+DISPLAY_LABEL=displayed-issue
+MAINTENANCE_LABEL=maintenance
+
+{
+    cd $DIR
+
+    rm -rf ./asset/json
+    mkdir -p ./asset/json ./asset/json/comments
+
+    # Grab all the issues we care about (maintenance and displayed-issues)
+    wget "https://api.github.com/repos/$GITHUB_REPOSITORY/issues?state=all&labels=$DISPLAY_LABEL" -O ./asset/json/displayed-issues.json
+    wget "https://api.github.com/repos/$GITHUB_REPOSITORY/issues?state=all&labels=$MAINTENANCE_LABEL" -O ./asset/json/maintenances.json
+    jq -s '.[][]' ./asset/json/displayed-issues.json ./asset/json/maintenances.json | jq -s -S 'sort_by(.number) | reverse' > ./asset/json/issues.json
+
+    for row in $(cat ./asset/json/issues.json | jq -r '.[] | @base64'); do
+        _jq() {
+            echo ${row} | base64 --decode | jq -r ${1}
+        }
+        comments=$(_jq '.comments')
+        if [[ $comments != 0 ]]; then
+            wget $(_jq '.comments_url') -O ./asset/json/comments/$(_jq '.number')-comments.json
+        fi
+    done    
+}
--- fetch_comments.sh
+++
@@ -1,8 +0,0 @@
-while IFS= read line; do
-    if [[ ! -z "$line" ]]; then
-        comments=$(jq --argjson line $line -r '.[] | select(.number==$line) | .comments' ./asset/json/issues.json)
-        if [[ "$comments" != "0" ]]; then
-            wget https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$line/comments -O ./asset/json/comments/$line-comments.json
-        fi
-    fi
-done <<< $(jq -r '.[].number' ./asset/json/issues.json)
